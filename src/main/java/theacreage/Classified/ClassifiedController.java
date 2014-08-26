0c03e7de84052119853843e6d1c340db3c24ebbe
Thomas Sonderman <trsonderm@gmail.com>
2014-08-26 17:31:11 -0400

    added classifieds 2

--- src/main/java/theacreage/Classified/ClassifiedController.java
+++ src/main/java/theacreage/Classified/ClassifiedController.java
@@ -1,7 +1,25 @@
 package theacreage.Classified;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import theacreage.Classified.Classified;
+import theacreage.BusinessListing.BusinessListingRepository;
+
+import java.util.List;
+
 /**
  * Created by wptrs on 8/26/2014.
  */
+@Controller
 public class ClassifiedController {
+    @Autowired
+    private ClassifiedRepository classifiedRepository;
+    @RequestMapping("/classifieds")
+    public String showClassifiedListings(Model model){
+        List<Classified> classifiedList = classifiedRepository.findAll();
+        model.addAttribute("listOfClassifieds", classifiedList);
+        return "classifiedListings";
+    }
 }
