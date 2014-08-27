6f393ebc508daeeea3c74ae68ac6584096356c1d
MacantaMonk <Remember00>
2014-08-26 22:11:18 -0400

    Cleared user to redo security

--- src/main/java/theacreage/User/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package theacreage.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by WPMWM on 8/26/2014.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, Integer>{
-}
--- src/main/java/theacreage/User/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package theacreage.User;
-
-/**
- * Created by wptrs on 8/26/2014.
- */
-public class UserRepository {
-}
