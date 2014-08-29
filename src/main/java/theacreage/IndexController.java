ecff6292b432882939c1fd1a4418befcb20582e7
MacantaMonk <Remember00>
2014-08-29 01:10:28 -0400

    Still security

--- src/main/java/theacreage/IndexController.java
+++ src/main/java/theacreage/IndexController.java
@@ -1,15 +1,33 @@
 package theacreage;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import theacreage.User.User;
 
 /**
  * Created by wptrs on 8/26/2014.
  */
 @Controller
+@RequestMapping("/")
 public class IndexController {
-    @RequestMapping("/")
-    public String showHomePage(){
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showHomePage(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object myUser = (auth != null) ? auth.getPrincipal() :  null;
+        System.out.println("test");
+
+        if (myUser instanceof User) {
+            System.out.println("test2");
+            User user = (User) myUser;
+            System.out.println(user.getUsername());
+            model.addAttribute("CurrentUser", user);
+        }
         return "index";
     }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,11 +1,12 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>thomas was here</title>
-
+    <title>The Acreage</title>
 </head>
 <body>
 <h1> testing</h1>
+<h1>${CurrentUser}</h1>
+<h3>${CurrentUser.username}</h3>
 </body>
 
 
